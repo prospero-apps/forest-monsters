812d6eb96d10cd23a4858bfcd7385513e5b5accd
prospero-apps <48125733+prospero-apps@users.noreply.github.com>
2023-04-13 18:42:31 +0200

    Create a Platform script and add it to the platform prefabs
    First we’ll create a Scripts folder and then in it we’ll create a Platform.cs script and attach it to each of the four Platform prefabs. In the script we’ll create a Mobility enum and add the basic variables. We’ll access the Rigidbody2D component in the Start method and we’ll handle the movement pattern of mobile platforms (from right to left to right, etc.) in the FixedUpdate method.

---
+++ Assets/Scripts/Platform.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Each platform is permanently immobile, permanently mobile or becomes
+/// mobile only when a character jumps onto it.
+/// </summary>
+public enum Mobility
+{
+    Immobile,
+    Mobile,
+    MobileOnEnter
+}
+
+public class Platform : MonoBehaviour
+{
+    // Is the platform mobile? Immobile? Mobile only when entered?
+    [SerializeField] private Mobility _mobility = Mobility.Immobile;
+
+    // How fast does the platform move from side to side if mobile.
+    [SerializeField] private float _speed = 3;
+
+    // Which direction is the platform moving in? Left or right?
+    [SerializeField] private float _direction = 1;
+
+    // How long does it take for a platform to start moving on player enter?
+    [SerializeField] private float _mobilityDelay = 1;
+
+    // Min and max x positions for the mobile platform
+    [SerializeField] private float _minX;
+    [SerializeField] private float _maxX;
+
+    private Rigidbody2D _rb2d;
+        
+    void Start()
+    {
+        _rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        if (_mobility == Mobility.Mobile)
+        {
+            // move right, then left, etc.
+            if (transform.position.x > _maxX && _direction > 0.0f) // turn back left
+            {
+                _direction *= -1;
+            }
+            if (transform.position.x < _minX && _direction < 0.0f) // turn back right
+            {
+                _direction *= -1;
+            }
+
+            transform.Translate(_speed * Time.deltaTime * _direction, 0, 0);
+        }
+    }
+}
